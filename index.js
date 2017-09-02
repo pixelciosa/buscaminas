6fddafeea0400723c91f563426e974cefbc853c4
Adriana <avillalobosfiatt@hotmail.com>
2017-09-02 19:21:51 -0300

    setting left click actions and the finalization and reloading of the game

--- index.js
+++ index.js
@@ -7,33 +7,33 @@ class Game {
 		this.mines = options.mines || 10
 		this.cellSize = options.cellSize || 20
 		this.isOver = false
+		this.buttons = []
 	}
 	init() {
 		var _this = this;
-
+		const canvas = document.getElementById('app')
 		// Set the grid container
 		function initCanvas() {
-			const canvas = document.getElementById('app')
 			canvas.style.width = (_this.cellSize * _this.cols)+'px'
 			canvas.style.height = (_this.cellSize * _this.rows)+'px'
 		}
 		// Create the cells with unique id
 		function initCells() {
 			var id = 0
+
 			for (let i = 0; i < _this.rows; i++) {
 
 				for (let i = 0; i < _this.cols; i++) {
 
-					var cell = document.createElement("button")
-					var cell_content = document.createTextNode("o")
-					cell.appendChild(cell_content)
+					var cell = document.createElement("cell")
+					cell.innerHTML =
+						`<button id="${id++}" class="btn" style="width:${_this.cellSize}px; height:${_this.cellSize}px "/>`
+
 					document.getElementById('app').appendChild(cell)
-					cell.style.width = _this.cellSize+'px'
-					cell.style.height = _this.cellSize+'px'
-					cell.setAttribute("id", id++);
 
 				}
 			}
+
 		}
 		// Put mines in random cells
 		function initMines() {
@@ -62,20 +62,67 @@ class Game {
 			for (var i = 0; i < minesLocations.length; i++) {
 				var mined = document.getElementById(minesLocations[i])
 
-				mined.className = "mined"
+				mined.classList.add('mined')
 
 				// Temp (For devs visualizacion)
 
-				mined.style.backgroundColor = 'red';
+				mined.style.opacity = '.9';
 
 			}
 		}
+		// Event Listeners
+		function eventListeners() {
+			_this.buttons = Array.from(document.getElementsByClassName('btn'))
+			console.log(_this.buttons)
+			_this.buttons.forEach(
+				function eventListeners(button) {
+					button.addEventListener('click', onClick)
+					button.addEventListener('contextmenu', rightClick, false)
+				}
+			)
+			function onClick() {
+				if (this.classList.contains('mined')) {
+					_this.endGame(this);
+				} else {
+					this.classList.add('open')
+				}
+			}
+			function rightClick(ev) {
+				 // ev.preventDefault();
+				console.log('ok right click')
+			    return false;
+			}
+		}
+
+
 
 		initCanvas();
 		initCells();
 		initMines();
+		eventListeners();
 
+	}
+	endGame(btn) {
+		var _this = this;
 
+		btn.classList.add('boom');
+		btn.style.backgroundColor = 'red'
+		for (var i=0; i < _this.buttons.length; i++) {
+		    if (_this.buttons[i].classList.contains('mined')) {
+		    	_this.buttons[i].classList.add('boom')
+		    } else {
+		    	_this.buttons[i].classList.add('open')
+		    };
+		}
+
+		setTimeout(function () {
+
+			alert('Perdiste :(')
+			_this.resetGame()
+		}, 200);
+	}
+	resetGame() {
+		document.location.href=""
 	}
 
 }
